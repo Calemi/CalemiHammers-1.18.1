46e60b6a3b3b64c346e68271e22de19549265e1d
Calemi <lemzsteamerson@gmail.com>
2022-02-09 21:32:41 -0500

    Changed how the tag lists are generated

--- src/main/java/com/tm/calemihammers/item/CHTagItemLists.java
+++ src/main/java/com/tm/calemihammers/item/CHTagItemLists.java
@@ -5,6 +5,9 @@ import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.versions.forge.ForgeVersion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,28 +19,29 @@ public class CHTagItemLists {
     public static final List<Item> oreTags = new ArrayList<>();
 
     public static boolean isLog(Block block) {
-
-        if (logTags.isEmpty()) {
-            logTags.addAll(ItemTags.LOGS.getValues());
-        }
-
         return Objects.requireNonNull(CHTagItemLists.logTags).contains(block.asItem());
     }
 
     public static boolean isOre(Block block) {
-
-        if (oreTags.isEmpty()) {
-            oreTags.addAll(ItemTags.getAllTags().getTag(new ResourceLocation(ForgeMod.getInstance().getModId(), "ores")).getValues());
-            oreTags.addAll(ItemTags.COAL_ORES.getValues());
-            oreTags.addAll(ItemTags.DIAMOND_ORES.getValues());
-            oreTags.addAll(ItemTags.COPPER_ORES.getValues());
-            oreTags.addAll(ItemTags.EMERALD_ORES.getValues());
-            oreTags.addAll(ItemTags.REDSTONE_ORES.getValues());
-            oreTags.addAll(ItemTags.LAPIS_ORES.getValues());
-            oreTags.addAll(ItemTags.GOLD_ORES.getValues());
-            oreTags.addAll(ItemTags.IRON_ORES.getValues());
-        }
-
         return Objects.requireNonNull(CHTagItemLists.oreTags).contains(block.asItem());
+    }
+
+    @SubscribeEvent
+    public static void onWorldLoad(WorldEvent.Load event) {
+
+        CHTagItemLists.logTags.clear();
+        CHTagItemLists.oreTags.clear();
+
+        CHTagItemLists.logTags.addAll(ItemTags.LOGS.getValues());
+
+        CHTagItemLists.oreTags.addAll(ItemTags.getAllTags().getTag(new ResourceLocation(ForgeVersion.MOD_ID, "ores")).getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.COAL_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.DIAMOND_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.COPPER_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.EMERALD_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.REDSTONE_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.LAPIS_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.GOLD_ORES.getValues());
+        CHTagItemLists.oreTags.addAll(ItemTags.IRON_ORES.getValues());
     }
 }
--- src/main/java/com/tm/calemihammers/main/CalemiHammers.java
+++ src/main/java/com/tm/calemihammers/main/CalemiHammers.java
@@ -4,9 +4,11 @@ import com.tm.calemihammers.config.CHConfig;
 import com.tm.calemihammers.event.SledgehammerChargeOverlayEvent;
 import com.tm.calemihammers.init.InitEnchantments;
 import com.tm.calemihammers.init.InitItems;
+import com.tm.calemihammers.item.CHTagItemLists;
 import com.tm.calemihammers.tab.CHTab;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -40,6 +42,9 @@ public class CalemiHammers {
 
         MOD_EVENT_BUS = FMLJavaModLoadingContext.get().getModEventBus();
         MOD_EVENT_BUS.addListener(this::onClientSetup);
+
+        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        forgeBus.addListener(EventPriority.NORMAL, CHTagItemLists::onWorldLoad);
 
         InitItems.init();
         InitEnchantments.init();
